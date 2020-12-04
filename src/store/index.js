8950b08cc932d0befa82f407b143e55b247e9797
alen <grafuxa@gmail.com>
2020-12-04 15:14:09 +0100

    added end point for projects

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,27 @@ import axios from "axios";
 Vue.use(Vuex, axios);
 
 export default new Vuex.Store({
- 
   state: {
     count: 1,
+    projectEndPoint:
+      "my-json-server.typicode.com/AlenJakob/vuetify-project/projects",
+  },
+  actions: {
+    async loadProducts({ commit }) {
+      await axios
+        .get(this.state.getProducts)
+        .then((result) => {
+          commit("LOAD_PRODUCTS", result.data);
+        })
+        .catch((error) => {
+          throw new Error(`API ${error}`);
+        });
+    },
   },
   mutations: {
     increment(state) {
       state.count++;
+      // does happend
     },
   },
 });
