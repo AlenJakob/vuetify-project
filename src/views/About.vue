496d9c69af83e6c279f6a3ec33f30622ca3425ed
Alen <33994452+AlenJakob@users.noreply.github.com>
2020-12-22 18:33:16 +0100

    about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
-<template>
-  <div class="about" max-width="500">
-    <h1>About</h1>
-    </div>
+
+ <template>
+  <v-container class="grey lighten-5">
+    <v-main>
+      <v-row no-gutters>
+        <v-col cols="12" sm="6">
+          <v-card class="pa-2" outlined tile> :) </v-card>
+        </v-col>
+        <v-col :key="n" cols="12" sm="6">
+          <v-card class="pa-2" outlined tile>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo nemo,
+            ipsam eum dignissimos eos, commodi mollitia nobis reprehenderit
+            impedit, cum minima voluptates facere recusandae numquam vel ipsa ex
+            velit voluptas?
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-main>
+  </v-container>
 </template>
+ 
