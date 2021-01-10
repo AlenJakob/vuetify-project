66f814d4f9ad7cf8234ea1af0e74427cfc55fac2
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-01-10 16:51:25 +0100

    contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,55 @@
 <template>
-  <div class="about" align="end">
+  <div class="about">
     <v-app-bar class="mb-1">
       <h4 class="subtitle-2">Contact</h4>
     </v-app-bar>
+    <v-divider></v-divider>
+    <v-container>
+      <v-row>
+        <v-col cols="6">
+          <v-card class="mx-auto" max-width="500">
+            <v-list>
+              <v-list-item-group>
+                <v-list-item
+                  v-for="(item, i) in contact"
+                  :key="i"
+                  :disabled="true"
+                >
+                  <v-list-item-icon>
+                    <v-icon v-text="item.icon"></v-icon>
+                  </v-list-item-icon>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title
+                        >{{ item.name }}:
+                        <b>{{ item.val }}</b></v-list-item-title
+                      >
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-card>
+        </v-col>
+        <v-col align="center" justify="center"><Inbox /></v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
+
+
+<script>
+import Inbox from "@/components/Inbox.vue";
+
+export default {
+  components: {
+    Inbox,
+  },
+  data: () => ({
+    contact: [
+      { name: "email", val: "alenjakob@gmail.com", icon: "mdi-email" },
+      { name: "mobile", val: "694754094", icon: "mdi-phone" },
+    ],
+  }),
+};
+</script>
